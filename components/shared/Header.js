c56a2df08d6c0b7b6c1b8e58b8f5d71da4bf25ec
thepunkprogrammer <mkodekar1990@gmail.com>
2019-01-11 08:35:00 +0530

    Add some styling

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,8 +1,8 @@
-/**
+ /**
  * @Author: webcubic3
  * @Date:   2019-01-10T16:14:43+05:30
- * @Last modified by:   webcubic3
- * @Last modified time: 2019-01-10T17:12:19+05:30
+ * @Last modified by:   rehank
+ * @Last modified time: 2019-01-11T02:50:48+05:30
  */
 
 
@@ -21,6 +21,7 @@ export class Header extends Component { // eslint-disable-line react/prefer-stat
       <>
       <a>{title}</a>
       {children}
+      <p class='customClass'>I am a styled p elemnts</p>
       <Link href="/">
         <a>Home</a>
       </Link>
@@ -36,6 +37,16 @@ export class Header extends Component { // eslint-disable-line react/prefer-stat
       <Link href="/about">
         <a>About</a>
       </Link>
+      <style jsx> {
+        `a {
+            font-size: 18px;
+            margin: 5px 5px;
+          };
+          .customClass {
+            color: red;
+          }`
+      }
+      </style>
       </>
     );
   }
