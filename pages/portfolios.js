f3e5fd796c9a64539a718c4e7c7556900490cc48
thepunkprogrammer <mkodekar1990@gmail.com>
2019-01-17 02:59:09 +0530

    Add dynamic routes

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -5,46 +5,45 @@
  * @Last modified time: 2019-01-12T15:18:02+05:30
  */
 
-
-
-import React, { Component } from 'react';
-import BaseLayout from '../components/layouts/BaseLayout'
-import axios from 'axios'
-import Link from 'next/link'
+import React, { Component } from "react";
+import BaseLayout from "../components/layouts/BaseLayout";
+import axios from "axios";
+import {Link} from "../routes";
 
 export default class Portfolios extends Component {
+  static async getInitialProps() {
+    let posts = [];
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      posts = response.data;
+    } catch (err) {
+      console.error(err);
+    }
 
-	static async getInitialProps () {
-		let posts = [];
-		try {
-			const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-			posts = response.data
-		} catch (err) {
-			console.error(err)
-		}
+    return { posts: posts.splice(0, 10) };
+  }
 
-		return {posts: posts.splice(0, 10)}
-	}
+  renderPosts(posts) {
+    return posts.map(post => (
+      <li key={post.id}>
+        <div key={post.id}>
+          <Link route={`/portfolio/${post.id}`}>
+            <a style={{ fontSize: "20px" }}>{post.title}</a>
+          </Link>
+        </div>
+      </li>
+    ));
+  }
 
-	renderPosts (posts) {
-		return posts.map(post => <li key={post.id}>
-				<div key={post.id}>
-					<Link as={`/portfolio/${post.id}`} href={`/portfolio?id=${post.id}`}>
-						<a style={{fontSize: '20px'}}>{post.title}</a>
-					</Link>
-					</div>
-		</li>)
-	}
-
-	render() {
-		const { posts } = this.props;
-		return (
-			<BaseLayout>
-				<h1>I am Portfolios page</h1>
-				<ul>
-					{this.renderPosts(posts)}
-				</ul>
-			</BaseLayout>
-		);
-	}
+  render() {
+    const { posts } = this.props;
+    return (
+      <BaseLayout>
+        <h1>I am Portfolios page</h1>
+        <ul>{this.renderPosts(posts)}</ul>
+      </BaseLayout>
+    );
+  }
 }
